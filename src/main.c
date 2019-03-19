e3294693700e4c3ee24ab8fca54b3d579b8bf940
mousinka <mousink@yandex.ru>
2019-03-19 10:36:15 +0300

    Настройка порта C и светодиода

--- src/main.c
+++ src/main.c
@@ -1,19 +1,23 @@
 /**
   ******************************************************************************
   * @file    main.c
-  * @author  Ac6
-  * @version V1.0
-  * @date    01-December-2013
-  * @brief   Default main function.
+  * @author  Bardina VA
+  * @version CMSIS
   ******************************************************************************
 */
 
-
 #include "stm32f10x.h"
-			
 
 int main(void)
 {
+	//Включаем тактирование порта C
+	RCC ->APB2ENR |= RCC_APB2ENR_IOPCEN;
+
+	//светодиод - порт C, пин13 (high - CRH)
+	//сброс состояния
+	//вывод светодиода как выход pull/push MODE=10: Max. output speed 2 MHz ; CRH=00:  General purpose output push-pull
+	GPIOC -> CRH &= ~(GPIO_CRH_CNF13_1 | GPIO_CRH_CNF13_0 | GPIO_CRH_MODE13_1 | GPIO_CRH_MODE13_0);
+	GPIOC -> CRH |= GPIO_CRH_MODE13_1;
 
 	for(;;);
 }
